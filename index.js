5a18a84bcc32c88b4e44d4dd948a1a991dff3d57
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-02-08 22:40:18 +0530

    Update post route to save data to data base from sigunp form

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 import express from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
 
 const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+dotenv.config();
 
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => { console.log("Connected to Database NotesDb"); })
+    .catch((err) => { console.log(err); });
 
+//User Model Schema 
+const usersSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+const User = mongoose.model('User', usersSchema);
+
+const userDetailSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Email',
+        required: true
+    }
+});
+const UserDetail = mongoose.model('UserDetail', userDetailSchema);
+
+// Get Route 
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
@@ -19,6 +55,46 @@ app.get("/signup", (req, res) => {
 app.get("/login", (req, res) => {
     res.render("login.ejs");
 })
+
+
+// Post Route
+
+app.post("/signup", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    // const confirmPassword = req.body.confirmPassword;
+    try {
+        const findEmail = await User.findOne({ email: email });
+        if (findEmail !== null) {
+            return res.render("signup.ejs", { emailError: "Email already exists" });
+        }
+        // if (password !== confirmPassword) {
+        //     return res.render("signup.ejs", { passError: "Passwords do not match" });
+        // }
+        // If email doesn't exist and passwords match, proceed to save the user
+        const newUser = new User({
+            email: email,
+            password: password
+        });
+        await newUser.save()
+            .then(() => {
+                console.log("Saved");
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        // Redirect to the login page after successful signup
+        res.redirect("/login");
+    } catch (error) {
+        console.log(error);
+        // Handle any errors that occur during the signup process
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+
+
 app.listen(port, (req, res) => {
     console.log(`server Started on port ${port}`);
 })
