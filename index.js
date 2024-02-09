d2a773abb2cba6f3a27715c15733080c45e76df5
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-02-09 12:04:19 +0530

    Successfully Signup and login system

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import session from "express-session";
 
 
 const port = 3000;
@@ -11,6 +14,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 const saltRounds = 10;
 dotenv.config();
+
+app.set("view engine", "ejs");
+app.use(session({
+    secret: "NOTESSECRET",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => { console.log("Connected to Database NotesDb"); })
@@ -49,7 +65,12 @@ const UserDetail = mongoose.model('UserDetail', userDetailSchema);
 // Get Route 
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    console.log(req.user);
+    if (req.isAuthenticated()) {
+        res.render("home.ejs");
+    } else {
+        res.redirect("/login");
+    }
 })
 app.get("/signup", (req, res) => {
     res.render("signup.ejs");
@@ -62,7 +83,7 @@ app.get("/login", (req, res) => {
 // Post Route
 
 app.post("/signup", async (req, res) => {
-    const email = req.body.email;
+    const email = req.body.username;
     const password = req.body.password;
     // const confirmPassword = req.body.confirmPassword;
     try {
@@ -94,8 +115,42 @@ app.post("/signup", async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 });
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+}));
 
 
+passport.use(new Strategy(async function verify(username, password, cb) {
+    try {
+        const user = await User.findOne({ email: username });
+        if (user === null) {
+            return cb("User Not Found")
+        } else {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (err) {
+                    return (cb(err));
+                }
+                if (user && result) {
+                    return (cb(null, user));
+                } else {
+                    return cb(null, false);
+                }
+            });
+        }
+    } catch (error) {
+        console.log(error)
+        res.send("Internal Server Error")
+    }
+}))
+
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
+});
 
 
 app.listen(port, (req, res) => {
